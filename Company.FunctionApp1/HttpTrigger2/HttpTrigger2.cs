c6eb9d49b9fa32218d2262188dc9a7f23ee8d74f
verlande <verlande@users.noreply.github.com>
2024-05-16 02:22:25 +0100

    HttpTrigger

--- Company.FunctionApp1/HttpTrigger2/HttpTrigger2.cs
+++ Company.FunctionApp1/HttpTrigger2/HttpTrigger2.cs
@@ -19,7 +19,12 @@ namespace Company.FunctionApp1
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            var name = req.Query["name"];
+            var ip = req.HttpContext.Connection.RemoteIpAddress?.ToString();
+            
+            return string.IsNullOrEmpty(name) || name.Count == 0
+                ? new BadRequestObjectResult("Please pass a name on the query string")
+                : new OkObjectResult($"Hello, {name} from {ip}");
         }
     }
 }
